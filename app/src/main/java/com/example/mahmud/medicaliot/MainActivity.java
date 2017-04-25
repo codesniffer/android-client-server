9f980d774a8dd16e467c432e80253e5942f8a18c
mahmud <mahmud>
2017-04-25 14:24:13 -0500

    android client-server

---
+++ app/src/main/java/com/example/mahmud/medicaliot/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.mahmud.medicaliot;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /** Called when the user touches the button */
+    public void sendMessage(View view) {
+        // Do something in response to button click
+        try {
+            EditText mEditIP = (EditText) this.findViewById(R.id.tb_payload);
+            EditText mEditBytes = (EditText) this.findViewById(R.id.tb_bytes);
+            Log.i("mIoT", "Button Clicketd: " + mEditIP.getText());
+
+            String serverIP = mEditIP.getText().toString();
+            int bytes = Integer.parseInt(mEditBytes.getText().toString());
+            new Sender().sendData(serverIP, 80, bytes);
+            System.out.println(serverIP);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/mahmud/medicaliot/Sender.java
@@ -0,0 +1,65 @@
+package com.example.mahmud.medicaliot;
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.io.InputStreamReader;
+
+class Sender{
+    String generateData (int sizeInBytes) {
+
+        int numberofBytes = sizeInBytes;
+
+        char [] _data = new char[sizeInBytes+1];
+
+        for(int i = 0; i< numberofBytes; i++) {
+            _data [i]= 'N';
+        }
+        return new String(_data);
+    }
+    void sendData(String serverIP, int serverPort, int size) throws InterruptedException {
+
+        System.out.println("Inside sendData Method");
+
+        String payload = generateData(size);
+        Thread t = new Thread(new SenderWorker(serverIP, serverPort,payload));
+        t.start();
+    }
+
+    class SenderWorker  implements Runnable  {
+
+        String serverAddress = null;
+        int serverPort = 0;
+        String payload = null;
+
+        SenderWorker(String serverAddress, int serverPort, String payload){
+            this.serverAddress = serverAddress;
+            this.serverPort = serverPort;
+            this.payload = payload;
+        }
+        @Override
+        public void run() {
+            try {
+                long startTime = System.nanoTime();
+
+                Socket socket = new Socket(this.serverAddress, this.serverPort);
+                //System.out.println("Sending data to " + socket.getInetAddress().getHostAddress() +":" + socket.getPort() );
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                out.println(this.payload);
+                String response = in.readLine();
+
+                long estimatedTime = System.nanoTime() - startTime;
+                Log.i("mIoT", "Response from Server: " + response +"; Time to receive: " + (int) (estimatedTime / 1000000) + " ms");
+                System.out.println("Response from Server: " + response +"; Time to receive: " + (int) (estimatedTime / 1000000) + " ms");
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
